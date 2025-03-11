56c156042ed3c35c86108ff7ae9c070c3b527cea
Hafemi <pascal.hafemi@gmail.com>
2025-03-11 09:53:03 +0100

    feat: slashcommand-subcommands component

---
+++ quippy/lib/bit-components/slashcommand-subcommands/index.ts
@@ -0,0 +1,3 @@
+export {
+  slashcommandSubcommands
+} from './slashcommand-subcommands.js';
---
+++ quippy/lib/bit-components/slashcommand-subcommands/slashcommand-subcommands.ts
@@ -0,0 +1,31 @@
+import {
+  ChatInputCommandInteraction,
+  InteractionContextType,
+  MessageFlags,
+  SlashCommandBuilder,
+  SlashCommandSubcommandsOnlyBuilder
+} from "discord.js";
+
+import * as InteractionHelper from "@cd/core.djs.interaction-helper";
+
+export const data: SlashCommandSubcommandsOnlyBuilder = new SlashCommandBuilder()
+  .setName('Name')
+  .setDescription('Description')
+  .setContexts([InteractionContextType.Guild])
+  .addSubcommand(subcommand => subcommand
+    .setName('example')
+    .setDescription('Example'));
+
+export async function execute(interaction: ChatInputCommandInteraction): Promise<void> {
+  const subcommand = interaction.options.getSubcommand();
+
+  if (subcommand == 'example') return await executeExample(interaction);
+
+  await InteractionHelper.followUp(interaction, `\`Error:\` Unknown subcommand '${subcommand}'`);
+}
+
+async function executeExample(interaction: ChatInputCommandInteraction): Promise<void> {
+  await interaction.deferReply({ flags: [MessageFlags.Ephemeral] });
+  
+  console.log('executed example');
+}
